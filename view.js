4b173bb731c2717355e4e97504c5af7969d075b4
Kyle Hill <kyle@everfi.com>
2012-03-16 00:15:02 -0400

    Mostly finished API/routing

--- view.js
+++ view.js
@@ -4,20 +4,29 @@ var async = require('async');
 var client = redis.createClient();
 var request = require('request');
 var parser = require('xml2json');
+var underscore = require('underscore');
 
-client.select(1, redis.print);
+client.select(1);
 
 app.get('/', function(req, res){
-  request('http://localhost:3001/stations', function(e, r, b) {
-    
+  res.sendfile("public/index.html");
+});
+
+app.get("/public/*", function(req, res){
+  res.sendfile("public/" + req.params);
+});
+
+app.get('/station/:id', function(req, res){
+  request({uri: 'http://' + req.headers.host + '/stationlist', json: true }, function(error, response, body) {
+    res.json(underscore.find(body.stations.station, function(s){ return s.terminalName == req.params.id; }));
   });
 });
 
-app.get('/stations', function(req, res){
+app.get('/stationlist', function(req, res){
   async.auto({
     getXml : function(callback) { 
       request('http://capitalbikeshare.com/data/stations/bikeStations.xml', function(error, response, body) {
-        callback(null, parser.toJson(body));
+        callback(null, parser.toJson(body, {object:true}));
       });
     },
     doStuff : [ 'getXml', function(callback, results) {
@@ -41,18 +50,34 @@ app.get('/from/:f/to/:t', function(req, res){
       });
     },
     getTrips : function(callback) {
-      client.zrange('from:' + f + ':to:' + t, 0, -1, 'withscores', function(err, res){
-        callback(null, tripDataToArray(res))
+      client.zrange('from:' + f + ':to:' + t, 0, -1, 'withscores', function(err, results){
+        var tripData = tripDataToArray(results);
+        appendTrip(tripData, 0, function() { callback(null, tripData); })
       });
     },
     sendResponse : [ 'getFromName', 'getToName', 'getTrips', function(callback, results) {
-      res.send(results);
+      res.json(results);
     }]
   });
 });
 
-var getStations = function(){
-  
+app.get('/trip/:id', function(req, res) {
+  var id = req.params.id;
+  client.hgetall("trip:" + id, function(err, results){
+    res.json(results);
+  });
+});
+
+var appendTrip = function(data, i, cb) {
+  client.hgetall("trip:" + data[i].id, function(err, results){
+    data[i]["trip"] = results;
+    if (i < data.length - 1) {
+      appendTrip(data, i+1, cb);    
+    }
+    else {
+      cb();
+    }
+  });
 }
 
 var tripDataToArray = function(obj){
